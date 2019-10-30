b1b320a258199242702f56bda95797027b4154ed
shaazihah <shaazihahseedat@gmail.com>
2019-10-30 13:21:16 +0200

    changed app.js

--- app.js
+++ app.js
@@ -123,9 +123,9 @@ app.delete("/favoritesBooks", (req, res) => {
 
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
